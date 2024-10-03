969ed6a6fbdd380f193be496128aafe9aea13465
Sainan <sainan@calamity.inc>
2024-10-03 23:25:47 +0200

    Oops, this is not valid Lua

--- reactivity.lua
+++ reactivity.lua
@@ -2,13 +2,15 @@ local reactive_vars = {}
 
 setmetatable(_G, {
     __index = function(self, key)
-        if info := reactive_vars[key] then
+        local info = reactive_vars[key]
+        if info then
             return info.value
         end
         return rawget(self, key)
     end,
     __newindex = function(self, key, value)
-        if info := reactive_vars[key] then
+        local info = reactive_vars[key]
+        if info then
             info.value = value
             info.fn(value)
         else
